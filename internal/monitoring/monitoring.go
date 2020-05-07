08d7952710c85dac4432064e1de65dbc601fe717
egorban <gorban@satissoft.ru>
2020-05-07 12:48:32 +0300

    fix

--- internal/monitoring/monitoring.go
+++ internal/monitoring/monitoring.go
@@ -31,6 +31,7 @@ var (
 	defaultPoint point
 	muConn       sync.Mutex
 	connsSystems map[string]uint64
+	host         string
 )
 
 func Init(address string, systems []util.System) (enable bool, err error) {
@@ -43,17 +44,10 @@ func Init(address string, systems []util.System) (enable bool, err error) {
 		logrus.Errorf("error while connecting to influx: %s\n", err)
 		return
 	}
-	host, err := getHost()
+	host, err = getHost()
 	if err != nil {
 		logrus.Errorf("error while getting host IP: %s\n", err)
 		return
-	}
-	defaultPoint = point{
-		tags: map[string]string{
-			"host":     host,
-			"instance": util.Instance,
-		},
-		values: make(map[string]string),
 	}
 	connsSystems = initSystemsConns(systems)
 	logrus.Infof("start sending metrics to influx to %+s:%+v, instance: %+s",
@@ -100,15 +94,21 @@ func DeleteConn(systemName string) {
 }
 
 func SendMetric(systemName string, metric string, count string) {
-	logrus.Infof("defaultPoint1 %v", defaultPoint)
-	newPoint := defaultPoint
+	var newPoint = point{
+		tags: map[string]string{
+			"host":     host,
+			"instance": util.Instance,
+		},
+		values: map[string]string{
+			metric: count,
+		},
+	}
 	if systemName != AttMonName {
 		newPoint.table = visTable
 	} else {
 		newPoint.table = attTable
 		newPoint.tags["system"] = systemName
 	}
-	newPoint.values[metric] = count
 	logrus.Infof("newPoint %v", newPoint)
 	record := newPoint.toRecord()
 	send(record)
